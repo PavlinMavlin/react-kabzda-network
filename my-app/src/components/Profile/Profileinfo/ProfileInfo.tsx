b923c7ffab513ca473b29ff4b251f90d98818162
PavlinMavlin <korobovpavel2017@yandex.by>
2021-06-24 14:31:16 +0300

    71 lesson

--- my-app/src/components/Profile/Profileinfo/ProfileInfo.tsx
+++ my-app/src/components/Profile/Profileinfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css"
 import {ProfileType} from "../../../Redux/profile-reducer";
+import ProfileStatus from "./ProfileStatus";
 
 type ProfileInfoPropsType = {
     profile: ProfileType
@@ -10,13 +11,13 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
     console.log(props)
     return (
         <div>
-            <div>
-                <img src={'https://img.as-creation.com/s0/031021.jpg'} alt={" "}/>
-            </div>
+            {/*<div>*/}
+            {/*    <img src={'https://img.as-creation.com/s0/031021.jpg'} alt={" "}/>*/}
+            {/*</div>*/}
 
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}   alt={" "}/>
-                ava+description
+                <ProfileStatus status={"hello pasha"}/>
             </div>
 
 
